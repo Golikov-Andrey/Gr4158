bab1d3198895a1f24f05480827ad4577f0921972
Golikov-Andrey <newsc2@yandex.ru>
2023-03-30 22:12:19 +0300

    add solution for task 17,18,20,22

---
+++ Sem3Task22/Program.cs
@@ -0,0 +1,22 @@
+﻿//Чтение данных консоли
+int ReadData(string message)
+{
+    Console.WriteLine(message);
+    int res = int.Parse(Console.ReadLine() ?? "0");
+    return res;
+}
+
+string LineGen(int num, int pow)
+{
+    string res = "";
+    for (int i = 1; i <= num; i++)
+    {
+        res += Math.Pow(i, pow) + " ";
+    }
+    return res;
+}
+
+int numN = ReadData("Введите чило N: ");
+Console.WriteLine(LineGen(numN,1));
+Console.WriteLine(LineGen(numN,2));
+
