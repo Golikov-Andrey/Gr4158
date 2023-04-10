e9ca23e8b7dd4abad59a03e407f18388516b4536
Golikov-Andrey <newsc2@yandex.ru>
2023-04-10 22:21:43 +0300

    add solution for task 38,39,40,42,44,45

---
+++ Sem5Task38/Program.cs
@@ -0,0 +1,23 @@
+﻿double[] Gen1DArray(int len, int min, int max)
+{
+    double[] arr = new double[len];
+    for (int i = 0; i < len; i++)
+    {
+        arr[i] = new Random().Next(min, max + 1) + new Random().NextDouble();
+    }
+    return arr;
+}
+
+//Метод, выводящий на печать массив
+void Print1DArr(double[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length - 1; i++)
+    {
+        Console.Write(arr[i] + ", ");
+    }
+    Console.WriteLine(arr[arr.Length - 1] + "]");
+}
+
+double[] tesatArray = Gen1DArray(123,0,50);
+Print1DArr(tesatArray);
